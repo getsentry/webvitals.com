f0e8bcf71ff5b7478072e2cf71a5fcbb22424273
Ben Vinegar <2153+benvinegar@users.noreply.github.com>
2024-04-03 11:30:34 -0400

    Remove unused code

--- src/app/demos/slow-inp/page.tsx
+++ src/app/demos/slow-inp/page.tsx
@@ -2,10 +2,6 @@ import INPButtons from './inpButtons';
 const FCP_DELAY = 2000; // ms
 
 export default async function Page() {
-
-    const clickButton = (speed) => {
-
-    }
     return (
         <div>
             <p>This demo demonstrates a slow INP (Interaction to First Paint).</p>
