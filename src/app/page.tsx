42545f01236f0ed8432602e956960a44d14805e1
Ben Vinegar <2153+benvinegar@users.noreply.github.com>
2024-05-23 10:38:53 -0400

    Add cards to landing page to point to demos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@
 import { useLoadState } from "./loadState";
 import { useEffect } from "react";
 
+import Link from "next/link";
+
 import {
   Card,
   CardContent,
@@ -25,53 +27,63 @@ export default function Home() {
       <h1 className="mb-8 text-3xl font-bold">Web Vitals Demos</h1>
 
       <div className="flex flex-wrap gap-4">
-        <Card className="flex-grow basis-1/3">
-          <CardHeader>
-            <CardTitle className="text-xl">Largest Contentful Paint (LCP)</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p>Largest Contentful Paint (LCP) measures the time it takes for the largest text or image element to render on a webpage.</p>
-          </CardContent>
-        </Card>
+        <Link className="flex-grow basis-1/3" href="/demos/lcp">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-xl">Largest Contentful Paint (LCP)</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>Largest Contentful Paint (LCP) measures the time it takes for the largest text or image element to render on a webpage.</p>
+            </CardContent>
+          </Card>
+        </Link>
 
-        <Card className="flex-grow basis-1/3">
-          <CardHeader>
-            <CardTitle className="text-xl">First Contentful Paint (FCP)</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p>
-              First Contentful Paint (FCP) measures the time from when a page starts loading to when any part of the page&apos;s content is first displayed.
-            </p>
-          </CardContent>
-        </Card>
+        <Link className="flex-grow basis-1/3" href="/demos/fcp">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-xl">First Contentful Paint (FCP)</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>
+                First Contentful Paint (FCP) measures the time from when a page starts loading to when any part of the page&apos;s content is first displayed.
+              </p>
+            </CardContent>
+          </Card>
+        </Link>
 
-        <Card className="flex-grow basis-1/3">
-          <CardHeader>
-            <CardTitle className="text-xl">Time to First Byte (TTFB)</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p>Time to First Byte (TTFB) measures the duration from when a page starts loading to when the first byte of content is received from the server.</p>
-          </CardContent>
-        </Card>
+        <Link className="flex-grow basis-1/3" href="/demos/ttfb">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-xl">Time to First Byte (TTFB)</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>Time to First Byte (TTFB) measures the duration from when a page starts loading to when the first byte of content is received from the server.</p>
+            </CardContent>
+          </Card>
+        </Link>
 
-        <Card className="flex-grow basis-1/3">
-          <CardHeader>
-            <CardTitle className="text-xl">Cumulative Layout Shift (CLS)</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p>        Cumulative Layout Shift (CLS) measures the total amount of unexpected layout shifts that occur during the entire lifespan of a webpage.</p>
-          </CardContent>
-        </Card>
+        <Link className="flex-grow basis-1/3" href="/demos/cls">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-xl">Cumulative Layout Shift (CLS)</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>Cumulative Layout Shift (CLS) measures the total amount of unexpected layout shifts that occur during the entire lifespan of a webpage.</p>
+            </CardContent>
+          </Card>
+        </Link>
 
 
-        <Card className="flex-grow basis-1/3">
-          <CardHeader>
-            <CardTitle className="text-xl">Interaction to Next Paint (INP)</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p>Interaction to Next Paint (INP) measures the time from when a user interacts with a page to when the browser renders the visual response to that interaction.</p>
-          </CardContent>
-        </Card>
+        <Link className="flex-grow basis-1/3" href="/demos/inp">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-xl">Interaction to Next Paint (INP)</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>Interaction to Next Paint (INP) measures the time from when a user interacts with a page to when the browser renders the visual response to that interaction.</p>
+            </CardContent>
+          </Card>
+        </Link>
       </div>
     </div>
   );
