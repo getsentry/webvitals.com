505192db4cce920073fab3dc640fd39ecea77a4b
Sergiy Dybskiy <sergiy.dybskiy@sentry.io>
2025-09-19 19:38:59 -0400

    no need for aiStore

--- lib/store.ts
+++
@@ -1,9 +0,0 @@
-// store.ts
-import { createAIStore } from "@ai-sdk-tools/store";
-
-export const store = createAIStore({
-  initialMessages: [],
-  onFinish: (message) => {
-    console.log("Message finished:", message);
-  },
-});
